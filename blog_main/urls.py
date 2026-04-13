09b088d2d2ce01cb85340f2af298a3aa0605e173
Shahriar Hasan <shahriar19735@gmail.com>
2026-04-13 21:40:12 +0600

    Add Category CRUD functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,11 +25,16 @@ from blogs import views as BlogsView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
     path("", views.home, name="home"),
     path("category/", include("blogs.urls")),
     path("blogs/<slug:slug>/", BlogsView.blogs, name="blogs"),
     path("search/", BlogsView.search, name="search"),
+    # authentication
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    # dashboard
+    path("dashboard/",include('dashboards.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
